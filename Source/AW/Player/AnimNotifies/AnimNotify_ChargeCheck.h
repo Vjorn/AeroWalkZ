1bfdd21a85b2ae15c621583f633b20ca70f9d836
Vjorn <mechdmc@gmail.com>
2020-08-01 15:17:20 +0300

    Anim Notifies - Charge Attacks

---
+++ Source/AW/Player/AnimNotifies/AnimNotify_ChargeCheck.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_ChargeCheck.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AW_API UAnimNotify_ChargeCheck : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
